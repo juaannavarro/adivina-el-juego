94f4870d5270a8a0295f4b1c84874a474fe51f74
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-11-05 17:11:21 +0100

    juego

--- adivinaelnumero.py
+++ adivinaelnumero.py
@@ -1,5 +1,14 @@
-
 import random 
     
-numero=random.randint(0,100)
-print("Numero aleatorio", numero)
+numero=random.randint(0,99)
+print("Introduzca el numero a adivinar", numero)
+while True:
+    numero=input("Introduzca un numero entre 0 y 99 incluidos")
+
+    try:
+        numero=int(numero)
+    except:
+        pass
+    else:
+        if 0<= numero <= 99:
+            break
